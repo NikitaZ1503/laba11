491a3675552772a90a9e58b4d0f168f7b1f57af3
N Z <144957203+NikitaZ1503@users.noreply.github.com>
2023-10-30 12:23:39 +0300

    Update isogram.cpp first commit

--- isogram/isogram.cpp
+++ isogram/isogram.cpp
@@ -1,14 +1,37 @@
-#include "isogram.hpp"
-#include <set>
-#include <locale>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <algorithm>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::vector;
+using std::tolower;
 
 // Для фукнций std::isalpha и std::toupper используйте эту локаль
 std::locale locale{""};
 
 auto is_isogram(std::string const& word) -> bool {
-    // Функция is_isogram получает на вход строку word
-    // Возвращает true, если строка является изограммой,
-    // и false, если не явялется
-    // Код пишите здесь
-    return word.empty();
+    sort(test.begin(), test.end());
+
+    vector<char> symbols;
+
+    for (int i = 0; i < test.size(); i++) {
+        int s = (int)(test[i]);
+        if (s >= 97 and s <= 122) {
+            symbols.push_back(test[i]);
+        }
+    }
+    for (int i = 0; i < symbols.size(); i++) {
+
+        if (tolower(symbols[i]) == tolower(symbols[i + 1])) {
+            return false;
+            break;
+        } else {
+            return true;
+        }
+    }
 }
